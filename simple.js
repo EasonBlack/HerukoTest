6645dc45f81e133d92c16bf4365b3d803544f7d8
easn xu <eason820720@gmail.com>
2016-07-14 15:03:21 +0800

    update

--- simple.js
+++ simple.js
@@ -1,12 +1,12 @@
 var express = require('express');
-
-
 var app = express();
-app.listen(3000, function(){
+app.set('port', (process.env.PORT || 5000));
+
+
+app.listen(app.get('port'), function(){
     console.log('connect success!');
 });
 
 app.get('/',function (req, res) {
     res.sendfile('./index.html');
 });
-
