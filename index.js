d71d765567e9a8456f0f9a26ef2b5cf81d5c7fba
easn xu <eason820720@gmail.com>
2016-07-14 16:59:04 +0800

    'u'

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
+var path = require('path');
+
 var app = express();
 app.set('port', (process.env.PORT || 5000));
 
-
+app.use(express.static(path.join(__dirname, 'build')));
 app.listen(app.get('port'), function(){
     console.log('connect success!');
 });
