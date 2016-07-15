c2b799cce8b60f5e9517c3ab1ea80dfb4fb6f780
easn xu <eason820720@gmail.com>
2016-07-15 15:42:51 +0800

    'a'

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var path = require('path');
 var pg = require('pg');
+var bodyParser = require('body-parser');
+var cors = require('cors')
 var client = new pg.Client(process.env.DATABASE_URL);
 
 var app = express();
 app.set('port', (process.env.PORT || 5000));
-
+app.use(bodyParser.json());
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.listen(app.get('port'), function(){
