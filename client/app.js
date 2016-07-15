4251f4afe5eb5802ef8e944eec142a9d87d3b054
easn xu <eason820720@gmail.com>
2016-07-15 15:17:31 +0800

    u

--- client/app.js
+++ client/app.js
@@ -1,6 +1,13 @@
 import 'angular';
 
 angular.module('app', [])
-    .controller('appCtrl', ['$scope', function ($scope) {
+    .controller('appCtrl', ['$scope', '$http', function ($scope,$http) {
         $scope.message = 'xxxd fd fdf';
+
+        $scope.add = ()=> {
+            $http.post('/add',{text: $scope.content}, function(err, res){
+               console.log(res);
+            });
+        }
+
     }]);
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var path = require('path');
+var pg = require('pg');
+var client = new pg.Client(process.env.DATABASE_URL);
 
 var app = express();
 app.set('port', (process.env.PORT || 5000));
@@ -13,3 +15,35 @@ app.listen(app.get('port'), function(){
 app.get('/',function (req, res) {
     res.sendfile('./index.html');
 });
+
+
+app.get('/db', function (request, response) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        client.query('SELECT * FROM test_table', function(err, result) {
+            done();
+            if (err)
+            { console.error(err); response.send("Error " + err); }
+            else
+            { response.send({results: result.rows}); }
+        });
+    });
+})
+
+app.post('/add',function(req, res){
+    var text = req.param('text');
+    client.query("INSERT INTO test_table(text) values($1)", [text]);
+    client.query('SELECT * FROM test_table', function(err, result) {
+        done();
+        if (err)
+        { console.error(err); response.send("Error " + err); }
+        else
+        { response.send({results: result.rows}); }
+    });
+});
+
+app.get('/createtable',function(){
+    //var client = new pg.Client(process.env.DATABASE_URL);
+    client.connect();
+    var query = client.query('CREATE TABLE test_table(id SERIAL PRIMARY KEY, text VARCHAR(40) not null)');
+    query.on('end', function() { client.end(); });
+})
