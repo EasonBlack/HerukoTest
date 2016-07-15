b499069b3118a7226edbdc56dd98c0ce407cee1b
easn xu <eason820720@gmail.com>
2016-07-15 17:54:32 +0800

    u

--- client/app.js
+++ client/app.js
@@ -4,14 +4,14 @@ angular.module('app', [])
     .controller('appCtrl', ['$scope', '$http', function ($scope,$http) {
         $scope.message = 'xxxd fd fdf';
 
-        $http.get('/getitems', function(err, res){
+        $http.get('/getitems', function(res){
             console.log(res);
             $scope.items = res;
         });
 
         $scope.add = ()=> {
             console.log($scope.content);
-            $http.post('/add',{text: $scope.content}, function(err, res){
+            $http.post('/add',{text: $scope.content}, function(res){
                 console.log(res);
             });
         }
