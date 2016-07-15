961daf8f85ee533af9357ab9175165b7bacb9f62
easn xu <eason820720@gmail.com>
2016-07-15 17:44:27 +0800

    y

--- client/app.js
+++ client/app.js
@@ -4,9 +4,15 @@ angular.module('app', [])
     .controller('appCtrl', ['$scope', '$http', function ($scope,$http) {
         $scope.message = 'xxxd fd fdf';
 
+        $http.get('/getitems', function(err, res){
+            console.log(res);
+            $scope.items = res;
+        });
+
         $scope.add = ()=> {
+            console.log($scope.content);
             $http.post('/add',{text: $scope.content}, function(err, res){
-               console.log(res);
+                console.log(res);
             });
         }
 
--- index.js
+++ index.js
@@ -19,26 +19,13 @@ app.get('/',function (req, res) {
     res.sendfile('./index.html');
 });
 
-
+//success
 app.get('/getitems',function(req,res){
     var client = new pg.Client(process.env.DATABASE_URL);
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
         client.query('SELECT * FROM test1', function(err, result) {
             done();
-            res.send(result);
-        });
-    });
-})
-
-app.get('/db', function (request, response) {
-    var client = new pg.Client(process.env.DATABASE_URL);
-    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-        client.query('SELECT * FROM test_table', function(err, result) {
-            done();
-            if (err)
-            { console.error(err); response.send("Error " + err); }
-            else
-            { response.send({results: result.rows}); }
+            res.send(result.rows);
         });
     });
 })
@@ -46,19 +33,9 @@ app.get('/db', function (request, response) {
 app.post('/add',function(req, res){
     var text = req.param('text');
     var client = new pg.Client(process.env.DATABASE_URL);
-    client.query("INSERT INTO test_table(text) values($1)", [text]);
-    client.query('SELECT * FROM test_table', function(err, result) {
+    client.query("INSERT INTO test1(text) values($1)", [text]);
+    client.query('SELECT * FROM test1', function(err, result) {
         done();
-        if (err)
-        { console.error(err); response.send("Error " + err); }
-        else
-        { response.send({results: result.rows}); }
+        res.send(result.rows);
     });
 });
-
-app.get('/createtable',function(){
-    var client = new pg.Client(process.env.DATABASE_URL);
-    client.connect();
-    var query = client.query('CREATE TABLE test_table(id SERIAL PRIMARY KEY, text VARCHAR(40) not null)');
-    query.on('end', function() { client.end(); });
-})
