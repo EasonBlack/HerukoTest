74f88fea4a35f93615c675486c2f53977ba661be
easn xu <eason820720@gmail.com>
2016-08-10 17:27:22 +0800

    asd

--- server.js
+++ server.js
@@ -52,8 +52,17 @@ app.post('/answer', function (req, res) {
     var username = req.body.username;
     var content = req.body.content;
     console.log(username);
-    console.log(content);
-    var insert = `insert into answer(username, content) values('${username}',ARRAY${content})`;
+
+    var _content = content.reduce((str, o, i)=>{
+        if(!i) {
+            str +=  '\'' + o +  '\'';
+        }  else {
+            str +=  ',\'' + o +  '\'';
+        }
+
+    }, '');
+    console.log(_content);
+    var insert = `insert into answer(username, content) values('${username}',ARRAY[${_content}])`;
     console.log(insert);
     var client = new pg.Client(process.env.DATABASE_URL);
     pg.connect(process.env.DATABASE_URL, function (err, client, done) {
